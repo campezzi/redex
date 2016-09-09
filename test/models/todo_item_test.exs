82963e3a832d53ae6f6dfda639744e4ccc08daaa
Thiago Campezzi <Thiago.ViccinoCampezzi@myob.com>
2016-09-09 15:39:25 +1000

    change wording in tests

--- test/models/todo_item_test.exs
+++ test/models/todo_item_test.exs
@@ -9,7 +9,7 @@ defmodule Redex.TodoItemTest do
     {:ok, context}
   end
 
-  test "TodoItemAdded sets the name and todo list references", %{todo_item: todo_item} do
+  test "applying TodoItemAdded sets the name and todo list references", %{todo_item: todo_item} do
     event = %TodoItemAdded{todo_list_id: "test-list", name: "Test Item"}
 
     todo_item = TodoItem.apply(todo_item, event)
@@ -18,7 +18,7 @@ defmodule Redex.TodoItemTest do
     assert todo_item.todo_list_id == "test-list"
   end
 
-  test "TodoItemCompleted sets done to true", %{todo_item: todo_item} do
+  test "applying TodoItemCompleted sets done to true", %{todo_item: todo_item} do
     event = %TodoItemCompleted{}
 
     todo_item = TodoItem.apply(todo_item, event)
--- test/models/todo_list_test.exs
+++ test/models/todo_list_test.exs
@@ -3,19 +3,17 @@ defmodule Redex.TodoListTest do
 
   alias Redex.{TodoList, TodoListCreated}
 
-  describe "applying events" do
-    setup do
-      context = %{todo_list: TodoList.new("test-list")}
+  setup do
+    context = %{todo_list: TodoList.new("test-list")}
 
-      {:ok, context}
-    end
+    {:ok, context}
+  end
 
-    test "TodoListCreated event sets the list name", %{todo_list: todo_list} do
-      event = %TodoListCreated{name: "Test List"}
+  test "applying TodoListCreated sets the list name", %{todo_list: todo_list} do
+    event = %TodoListCreated{name: "Test List"}
 
-      todo_list = TodoList.apply(todo_list, event)
+    todo_list = TodoList.apply(todo_list, event)
 
-      assert todo_list.name == "Test List"
-    end
+    assert todo_list.name == "Test List"
   end
 end
